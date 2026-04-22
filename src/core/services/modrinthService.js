24f18a49c21aec881c7bca3540da0c8f9e507a26
f516122 <f516122@corp.caixa.gov.br>
2026-04-21 23:39:08 -0300

    ajustes

--- src/core/services/modrinthService.js
+++ src/core/services/modrinthService.js
@@ -1,3 +1,4 @@
+const { tr } = require("date-fns/locale");
 const { httpModrinth, httpDiscord } = require("../../config/http");
 const { saveCache, loadCache } = require("../../utils/cache");
 const PROJECT = process.env.PROJECTS_MODRINTH;
@@ -16,7 +17,7 @@ class ProjectService {
             resolve(res.data);
         })
     }
-
+    
     getDataFromProject(project) {
         return new Promise(async (resolve, reject) => {
             const res = await httpModrinth.get(
@@ -27,63 +28,74 @@ class ProjectService {
     }
     getDataFromProjectVersion(project) {
         return new Promise(async (resolve, reject) => {
-            const res = await httpModrinth.get(
+            httpModrinth.get(
                 `/project/${project}/version`
-            );
-            resolve(res.data);
+            ).then(res => resolve(res.data))
+            .catch(err => reject(err));
         })
     }
-
+    
     fetchProjects(){
         return new Promise(async(resolve, reject) => {
-            const res = await this.getDataFromMultipleProjects( JSON.parse( PROJECT ) );
-            resolve(res);
+            try {
+                const res = await this.getDataFromMultipleProjects( JSON.parse( PROJECT ) );
+                resolve(res);
+            } catch (error) {
+                reject(error)   
+            }
         });
     }
     
     checkUpdates() {
         return new Promise(async(resolve, reject) => {
             const oldCache = loadCache();
-            const projects = await this.fetchProjects();
-            let newCache = {};
-            for (const project of projects) {
-                newCache[project.id] = project.updated;
-                console.log(`Verificando atualizações para ${project.title}`);
-                
-                
-                // Novo projeto
-                if (!oldCache[project.id]) {
-                    console.log('Novo');
-                    await this.sendDiscord(`🆕 Novo modpack: ${project.title}`);
+            try {
+                const projects = await this.fetchProjects();
+                let newCache = {};
+                for (const project of projects) {
+                    newCache[project.id] = project.updated;
+                    console.log(`Verificando atualizações para ${project.title}`);
+                    
+                    
+                    // Novo projeto
+                    if (!oldCache[project.id]) {
+                        console.log('Novo');
+                        //await this.sendDiscord(`🆕 Novo modpack: ${project.title}`);
+                    }
+                    
+                    // // Atualização
+                    else if (oldCache[project.id] !== project.updated) {
+                        console.log('Atualização');
+                        //await this.sendDiscord(`🔄 Modpack atualizado: ${project.title}`);
+                    }
                 }
                 
-                // // Atualização
-                 else if (oldCache[project.id] !== project.updated) {
-                    console.log('Atualização');
-                    await this.sendDiscord(`🔄 Modpack atualizado: ${project.title}`);
-                 }
+                //   // Removidos
+                for (const id in oldCache) {
+                    if (!newCache[id]) { 
+                        console.log(`Removido: ${id}`);
+                        //await this.sendDiscord(`❌ Modpack removido: ${id}`);
+                    }
+                }
+                
+                saveCache(newCache);
+                
+                resolve(newCache)
+            } catch (error) {
+                console.log('checkUpdates error', error);
+                reject(error);
             }
             
-            //   // Removidos
-            for (const id in oldCache) {
-                if (!newCache[id]) { 
-                    await this.sendDiscord(`❌ Modpack removido: ${id}`);
-                    console.log(`Removido: ${id}`);
-                }
-            }
-
-            saveCache(newCache);
-
-            resolve(newCache)
         });
     }
-
+    
     sendDiscord(message) {
         return new Promise(async(resolve, reject) => {
-            await httpDiscord.post("/", {
+            
+            httpDiscord.post("/", {
                 content: message
-            });
-            resolve();
+            }).then(() => resolve({ success: true }))
+            .catch(err => reject(err));
         });
     }
     
